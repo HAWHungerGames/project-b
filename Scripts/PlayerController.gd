b9711c4c2cc56542c80bcfbd0c609ae1cedd5db0
Helmni <120278094+Helmni@users.noreply.github.com>
2025-03-25 18:08:24 +0100

    Dash mechanic and Stamina mechanic
    -Dash mechanic with either a cooldown or a stamina cost
    -Stamina mechanic that recovers over time

--- Scripts/PlayerController.gd
+++ Scripts/PlayerController.gd
@@ -4,14 +4,23 @@ extends CharacterBody3D
 @export var speed = 5.0
 @export var acceleration = 4.0
 @export_range(0, 1) var sensitivity: float = 1
-@export_enum("rotate based on last movement", "rotate based on relative mouse position", "rotation based on right joystick") var rotation_type: String = "rotate based on last movement"
-@export var lock_active: bool = true
+@export_enum("rotate based on last movement", "rotate based on relative mouse position", "rotation based on right joystick") var rotationType: String = "rotate based on last movement"
+@export var lockActive: bool = true
+@export var dashStrength: int = 200
+@export var dashMaxCooldown: float = 3
+@export var maxStamina: int = 200
+@export var staminaPerSecond: int = 10
+@export var staminaCostPerDash: int = 50
+@export_enum("dash with cooldown","dash with stamina") var dashType: String = "dash with cooldown"
 
 @export_category("Components")
 @export var playerShape: CollisionShape3D
 
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 var temprotation = 0
+var dashCooldown: float = 0
+var stamina: int = maxStamina
+var staminaTimer: float = 0
 
 @onready var spring_arm = $CameraArm
 
@@ -21,9 +30,47 @@ func _physics_process(delta):
 	get_move_input(delta)
 	move_and_slide()
 	rotate_player()
+	dash(delta)
+	stamina_system(delta)
+
+func dash(delta):
+	match dashType:
+		"dash with cooldown":
+			dash_with_cooldown(delta)
+		"dash with stamina":
+			dash_with_stamina(delta)
+
+func dash_with_cooldown(delta):
+	if Input.is_action_just_pressed("dash") and dashCooldown <= 0:
+		dash_ability(delta)
+		dashCooldown = dashMaxCooldown
+	if dashCooldown >= 0:
+		dashCooldown = dashCooldown - delta
+
+func dash_with_stamina(delta):
+	if Input.is_action_just_pressed("dash") and stamina >= staminaCostPerDash:
+		dash_ability(delta)
+		stamina -= staminaCostPerDash
+
+func stamina_system(delta):
+	if stamina < maxStamina:
+		if staminaTimer >= 1:
+			stamina += staminaPerSecond
+			staminaTimer = 0
+			print(stamina)
+		else:
+			staminaTimer += delta
+
+func dash_ability(delta):
+	var vy = velocity.y
+	velocity.y = 0
+	var input = Input.get_vector("left", "right", "forward", "backward").normalized()
+	var direction = Vector3(input.x, 0, input.y).rotated(Vector3.UP, spring_arm.rotation.y)
+	velocity = lerp(velocity, direction * dashStrength, acceleration * delta)
+	velocity.y = vy
 
 func rotate_player():
-	match rotation_type:
+	match rotationType:
 		"rotate based on last movement":
 			rotate_based_on_last_movement()
 		"rotate based on relative mouse position":
@@ -34,7 +81,7 @@ func rotate_player():
 func rotate_based_on_last_movement():
 	var input = Input.get_vector("left", "right", "forward", "backward")
 	var lock = false
-	if lock_active:
+	if lockActive:
 		lock = Input.is_action_pressed("lock_movement")
 	if (input.x != 0 or input.y != 0) and !lock:
 		temprotation = atan2(-input.x, -input.y)
@@ -49,17 +96,20 @@ func rotate_based_on_relative_mouse_position():
 	playerShape.rotation.y = mouseAngle
 
 func rotation_based_on_right_joystick():
-	var input = Input.get_vector("look_left", "look_right", "look_forward", "look_backward")
+	var input = Input.get_vector("left", "right", "forward", "backward")
 	if (input.x != 0 or input.y != 0):
 		temprotation = atan2(-input.x, -input.y)
+	var look_input = Input.get_vector("look_left", "look_right", "look_forward", "look_backward")
+	if (look_input.x != 0 or look_input.y != 0):
+		temprotation = atan2(-look_input.x, -look_input.y)
 	playerShape.rotation.y = temprotation
 
 func get_move_input(delta):
 	var vy = velocity.y
 	velocity.y = 0
 	var input = Input.get_vector("left", "right", "forward", "backward").normalized()
-	var dir = Vector3(input.x, 0, input.y).rotated(Vector3.UP, spring_arm.rotation.y)
-	velocity = lerp(velocity, dir * speed, acceleration * delta)
+	var direction = Vector3(input.x, 0, input.y).rotated(Vector3.UP, spring_arm.rotation.y)
+	velocity = lerp(velocity, direction * speed, acceleration * delta)
 	velocity.y = vy
 
 func apply_gravity(delta):
