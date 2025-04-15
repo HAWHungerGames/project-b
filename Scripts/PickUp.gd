f67c4eab2033e4daf21448e2cd181fe4442f89b3
KonradSmyk <wgc312@haw-hamburg.de>
2025-04-15 10:37:06 +0200

    socket script

--- Scripts/PickUp.gd
+++ Scripts/PickUp.gd
@@ -1,14 +1,14 @@
 extends RigidBody3D
 
-@onready var sword = $"."
+@onready var weapon = $"."
 
 var interactable = true
 
 func interactWeapon(hand: Object):
 	if interactable == true:
 		interactable = false
-		sword.get_parent().remove_child(sword)
-		hand.add_child(sword)
-		sword.global_transform = hand.global_transform
-		sword.collision_layer = 2
+		weapon.get_parent().remove_child(weapon)
+		hand.add_child(weapon)
+		weapon.global_transform = hand.global_transform
+		weapon.collision_layer = 2
 		print("hallo")
---
+++ Scripts/socket.gd
@@ -0,0 +1,22 @@
+extends RigidBody3D
+
+@export_category("Items")
+@export_subgroup("Weapons")
+@export_enum("sword", "staff", "bow") var weapon: String = "sword"
+
+@onready var attachment = $"Weapon Slot"
+@onready var sword = preload("res://Scenes/placeholder_sword.tscn")
+@onready var staff = preload("res://Scenes/placeholder_staff.tscn")
+@onready var bow = preload("res://Scenes/placeholder_bow.tscn")
+
+func _ready() -> void:
+	match weapon:
+		"sword":
+			var sword_intance = sword.instantiate()
+			attachment.add_child(sword_intance)
+		"staff":
+			var staff_intance = staff.instantiate()
+			attachment.add_child(staff_intance)
+		"bow":
+			var bow_intance = bow.instantiate()
+			attachment.add_child(bow_intance)
