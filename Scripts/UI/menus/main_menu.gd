bf96168dfe5670a448857d02fef0ac442c4501cc
RoboSG <s.gooss@web.de>
2025-05-21 14:08:34 +0200

    menus. deletet weapon script (broken)

--- Scripts/UI/menus/main_menu.gd
+++ Scripts/UI/menus/main_menu.gd
@@ -4,6 +4,7 @@ extends Control
 @onready var vbox = $MarginContainer/VBoxContainer
 @onready var background = $MarginContainer/titleBackground
 
+@onready var timer = $MarginContainer/VideoStreamPlayer/Timer
 @onready var buttonSelectRightStart = $MarginContainer/VBoxContainer/MarginContainer/NinePatchRect/MarginContainer/VBoxContainer/HBoxContainer/buttonSelectRight
 @onready var buttonSelectLeftStart = $MarginContainer/VBoxContainer/MarginContainer/NinePatchRect/MarginContainer/VBoxContainer/HBoxContainer/buttonSelectLeft
 @onready var buttonSelectRightOptions = $MarginContainer/VBoxContainer/MarginContainer/NinePatchRect/MarginContainer/VBoxContainer/HBoxContainer3/buttonSelectRight
@@ -11,40 +12,47 @@ extends Control
 @onready var buttonSelectRightExit = $MarginContainer/VBoxContainer/MarginContainer/NinePatchRect/MarginContainer/VBoxContainer/HBoxContainer2/buttonSelectRight
 @onready var buttonSelectLeftExit = $MarginContainer/VBoxContainer/MarginContainer/NinePatchRect/MarginContainer/VBoxContainer/HBoxContainer2/buttonSelectLeft
 
-
-
-
 func _ready():
+	SceneManager.main_scene.connect(fadeSceneIn)
 	fadeSceneIn()
 
 func _on_button_pressed() -> void:
+	fadeSceneOut()
+	await get_tree().create_timer(2).timeout
 	get_tree().change_scene_to_file("res://Scenes/Nick Work Scene.tscn")
 
 
 func _on_button_3_pressed() -> void:
+	fadeSceneOut()
+	await get_tree().create_timer(2).timeout
 	get_tree().quit()
 
 func fadeSceneIn():
+	print("scene")
+	timer.start()
+	print("timer")
 	var tween = create_tween()
-	tween.tween_property(color_rect, "modulate:a", 0, 6.0)
+	tween.tween_property(color_rect, "modulate:a", 0, 4.0)
 	tween.play()
 	
 func fadeSceneOut():
-	var tween = create_tween()
-	tween.tween_property(color_rect, "modulate:a", 1.0, 6.0)
-	tween.play()
 	color_rect.process_mode = Node.PROCESS_MODE_INHERIT
+	var tween = create_tween()
+	tween.set_parallel()
+	tween.tween_property(vbox, "modulate:a", 0.0, 1.0)
+	tween.tween_property(color_rect, "modulate:a", 1.0, 1.0)
+	tween.play()
+	
 func fadeIn():
 	# Start with the container invisible
 	# Create a tween
 	var tween = create_tween()
-	var tween2 = create_tween()
+	tween.set_parallel()
 	tween.tween_property(vbox, "modulate:a", 1.0, 1.0)
-	tween2.tween_property(background, "modulate:a", 1.0, 1.0)
+	print("out?")
+	tween.tween_property(background, "modulate:a", 1.0, 1.0)
 	tween.play()
-	tween2.play()
 	color_rect.process_mode = Node.PROCESS_MODE_DISABLED
-
 
 func _on_timer_timeout() -> void:
 	fadeIn()
--- Scripts/UI/menus/pause_menu.gd
+++ Scripts/UI/menus/pause_menu.gd
@@ -2,27 +2,88 @@ extends MarginContainer
 
 @onready var pause_menu = $pauseMenuContainer
 
+@onready var buttonSelectRightStart = $pauseMenuContainer/MarginContainer/NinePatchRect/buttonMargin/buttons/buttonsResume/buttonSelectRight
+@onready var buttonSelectLeftStart = $pauseMenuContainer/MarginContainer/NinePatchRect/buttonMargin/buttons/buttonsResume/buttonSelectLeft
+@onready var buttonSelectRightOptions = $pauseMenuContainer/MarginContainer/NinePatchRect/buttonMargin/buttons/buttonsOptions/buttonSelectRight
+@onready var buttonSelectLeftOptions = $pauseMenuContainer/MarginContainer/NinePatchRect/buttonMargin/buttons/buttonsOptions/buttonSelectLeft
+@onready var buttonSelectRightExit = $pauseMenuContainer/MarginContainer/NinePatchRect/buttonMargin/buttons/buttonsExit/buttonSelectRight
+@onready var buttonSelectLeftExit = $pauseMenuContainer/MarginContainer/NinePatchRect/buttonMargin/buttons/buttonsExit/buttonSelectLeft
+@onready var blendScreen = $"../ColorRect"
+
+
 func _input(event):
-	if Input.is_action_just_pressed("pause"):
-		if pause_menu.process_mode == Node.PROCESS_MODE_INHERIT:
+	if event.is_action_pressed("pause"):
+		print("pause")
+		if pause_menu.process_mode == Node.PROCESS_MODE_ALWAYS:
 			unpause()
 		else:
 			pause()
 
 
-func _on_button_3_pressed() -> void:
-	get_tree().quit()
+func _on_exit_pressed() -> void:
+	SceneManager.main_scene.emit()
+	var tween = create_tween()
+	tween.tween_property(blendScreen, "modulate:a", 1.0, 1.0)
+	tween.play()
+	pause_menu.z_index = 0
+	await get_tree().create_timer(2).timeout
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://Scenes/TitleScreen.tscn")
 
 
-func _on_continue_pressed() -> void:
+func _on_resume_pressed() -> void:
 	unpause()
 
+func fadeSceneIn():
+	var tween = create_tween()
+	tween.tween_property(blendScreen, "modulate:a", 0.4, 0.1)
+	tween.play()
+	
+func fadeSceneOut():
+	var tween = create_tween()
+	tween.tween_property(blendScreen, "modulate:a", 0.0, 0.1)
+	tween.play()
+	process_mode = Node.PROCESS_MODE_INHERIT
+	
 func unpause():
+	fadeSceneOut()
 	pause_menu.visible = false
+	pause_menu.z_index = 0
 	pause_menu.process_mode =  Node.PROCESS_MODE_DISABLED
 	get_tree().paused = false
 	
 func pause():
+	fadeSceneIn()
 	pause_menu.visible = true
+	pause_menu.z_index = 3
 	pause_menu.process_mode = Node.PROCESS_MODE_ALWAYS
 	get_tree().paused = true
+	
+func _on_resume_hover_enter() -> void:
+	buttonSelectLeftStart.modulate.a = 1.0
+	buttonSelectRightStart.modulate.a = 1.0
+
+
+func _on_resume_hover_exit() -> void:
+	buttonSelectLeftStart.modulate.a = 0.0
+	buttonSelectRightStart.modulate.a = 0.0
+
+
+func _on_options_hover_enter() -> void:
+	buttonSelectLeftOptions.modulate.a = 1.0
+	buttonSelectRightOptions.modulate.a = 1.0
+
+
+func _on_options_hover_exit() -> void:
+	buttonSelectLeftOptions.modulate.a = 0.0
+	buttonSelectRightOptions.modulate.a = 0.0
+
+
+func _on_exit_hover_enter() -> void:
+	buttonSelectLeftExit.modulate.a = 1.0
+	buttonSelectRightExit.modulate.a = 1.0
+
+
+func _on_exit_hover_exit() -> void:
+	buttonSelectLeftExit.modulate.a = 0.0
+	buttonSelectRightExit.modulate.a = 0.0
