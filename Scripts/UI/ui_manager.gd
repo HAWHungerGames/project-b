5b27f07e5990a142a64833e336bb12e67e700ce3
RoboSG <s.gooss@web.de>
2025-05-24 17:49:56 +0200

    text beginning, interactable keys

--- Scripts/UI/ui_manager.gd
+++ Scripts/UI/ui_manager.gd
@@ -9,6 +9,35 @@ const LANGUAGES : Dictionary = {
 	"en_AU": "SETTING_LANGUAGE_ENGLISH_AU",
 	"de_DE": "SETTING_LANGUAGE_GERMAN"
 }
+const button_mappings = {
+	"xbox": {
+		"interact": "A",
+		"block": "LT",
+		"pause": "Menu",
+		"attack": "RT",
+		"weapon_swap": "Y",
+		"sneak": "L3",
+		"dash": "LB"
+	},
+	"ps5": {
+		"interact": "✕",
+		"block": "L2", 
+		"pause": "Start",
+		"attack": "R2",
+		"weapon_swap": "△",
+		"sneak": "L3",
+		"dash": "L1"
+	},
+	"nintendo": {
+		"interact": "B",
+		"block": "ZL",
+		"menu": "+",
+		"attack": "ZR",
+		"weapon_swap": "X",
+		"sneak": "L3",
+		"dash": "L"
+	}
+}
 
 signal lang_change
 
@@ -20,13 +49,13 @@ func get_controller_type():
 	var controller_type = "None" 
 	if len(Input.get_connected_joypads()) > 0:
 		if "ps" in Input.get_joy_name(0).to_lower():
-			controller_type = "PS5_"
+			controller_type = "PS5"
 		elif "nintendo" in Input.get_joy_name(0).to_lower():
-			controller_type = "Nintendo_"
+			controller_type = "Nintendo"
 		elif "xbox" in Input.get_joy_name(0).to_lower():
-			controller_type = "XBox_"
+			controller_type = "XBox"
 	return controller_type
 
 # future changes - generic also to keyboard
-func get_controller_input_key(action, controller_connected):
-	action.
+func get_controller_input_key(action, controller_connected) -> String:
+	return button_mappings[current_controller.to_lower()[action]]
