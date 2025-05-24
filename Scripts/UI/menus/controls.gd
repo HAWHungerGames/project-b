801bf2f1fba63b44d775e2f2ea52c416c531f1ce
RoboSG <s.gooss@web.de>
2025-05-24 16:22:49 +0200

    menu nav

--- Scripts/UI/menus/controls.gd
+++ Scripts/UI/menus/controls.gd
@@ -3,10 +3,14 @@ extends Control
 @onready var controller_text = $MarginContainer/controlsMenuContainer/MarginContainer/titles/HBoxContainer/controller
 @onready var keyboard_text = $MarginContainer/controlsMenuContainer/MarginContainer/titles/HBoxContainer/keyboard
 @onready var controller_menu = $MarginContainer/controlsMenuContainer/MarginContainer/Controller
-@onready var controller_image = $MarginContainer/controlsMenuContainer/MarginContainer/Controller/TextureRect
+@onready var controller_image = $MarginContainer/controlsMenuContainer/MarginContainer/Controller/inputs
+@onready var controller_label = $MarginContainer/controlsMenuContainer/MarginContainer/Controller/connect_controller
 @onready var keyboard_menu = $MarginContainer/controlsMenuContainer/MarginContainer/keyboard
 @onready var button_left = $MarginContainer/controlsMenuContainer/MarginContainer/titles/HBoxContainer/buttonLeft
 @onready var button_right = $MarginContainer/controlsMenuContainer/MarginContainer/titles/HBoxContainer/buttonRight
+@onready var back = $MarginContainer/controlsMenuContainer/MarginContainer/NinePatchRect/buttonMargin/buttons/buttonsBack
+
+@onready var pause_menu = $"../../pauseMenu"
 
 const CONTROLLER_CONTROLS: Dictionary = {
 	"PS5_en_EN": "res://UI/Menus/Controller/PS5_en_EN.png",
@@ -17,13 +21,39 @@ const CONTROLLER_CONTROLS: Dictionary = {
 	"Nintendo_de_DE": "res://UI/Menus/Controller/Nintendo_de_DE.png",
 }
 
+var controller = false
+var control_buttons = false
+
 func _ready() -> void:
 	UiManager.lang_change.connect(update_controller_controls)
-	
+
+func _input(event: InputEvent) -> void:
+	if !visible:
+		return
+	if event.is_action_pressed("ui_up"):
+		control_buttons = true
+		if controller:
+			button_right.grab_focus()
+		else:
+			button_left.grab_focus()
+	elif event.is_action_pressed("ui_down"):
+		control_buttons = false
+		back.get_child(1).grab_focus()
+	elif event.is_action_pressed("ui_left"):
+		if !control_buttons:
+			return
+		print("button left")
+		button_left.grab_focus()
+	elif event.is_action_pressed("ui_right"):
+		if !control_buttons:
+			return
+		print("button right")
+		button_right.grab_focus()
+
 func update_controller_controls() -> void:
 	var controller_type = "XBox_" 
 	if len(Input.get_connected_joypads()) > 0:
-		button_right.grab_focus()
+		controller = true
 		reveal_controller()
 		if "ps" in Input.get_joy_name(0).to_lower():
 			controller_type = "PS5_"
@@ -31,10 +61,16 @@ func update_controller_controls() -> void:
 			controller_type = "Nintendo_"
 		else:
 			controller_type = "XBox_"
+		controller_image.visible == true
+		controller_label.visible == false
 		controller_image.texture = load(CONTROLLER_CONTROLS[controller_type + UiManager.current_lang])
 	else:
-		button_left.grab_focus()
+		controller = false
+		controller_image.visible == false
+		controller_label.visible == true
 		reveal_keyboard()
+	back.get_child(1).grab_focus()
+		
 func _on_button_left_pressed() -> void:
 	reveal_controller()
 
@@ -52,3 +88,9 @@ func reveal_controller() -> void:
 	controller_menu.visible = true
 	keyboard_menu.visible = false
 	keyboard_text.visible = false
+
+func _on_back_mouse_entered() -> void:
+	pause_menu.toggle_button_selects(back, true)
+
+func _on_back_mouse_exited() -> void:
+	pause_menu.toggle_button_selects(back, false)
