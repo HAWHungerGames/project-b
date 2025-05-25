6ab5ab87c3458984b22c7b99d126c6298a3c6818
xxPigelxx <129988942+xxPigelxx@users.noreply.github.com>
2025-05-25 15:47:47 +0200

    Background music

--- Scripts/Assets/Door.gd
+++ Scripts/Assets/Door.gd
@@ -2,14 +2,21 @@ extends StaticBody3D
 
 @onready var ani_player = $AnimationPlayer
 @onready var fallingParticles = $FallingParticles
+@onready var audio = $AudioStreamPlayer3D
+
 var isClosed = true
+
 
 func _physics_process(delta: float) -> void:
 	if GameManager.get_weapon_in_hand() and isClosed:
 		fallingParticles.emitting = false
 		ani_player.play("Opening")
+		audio.set_pitch_scale(0.2)
+		audio.play()
 		isClosed = false
 	elif !GameManager.get_weapon_in_hand() and !isClosed:
 		fallingParticles.emitting = true
 		ani_player.play("Closing")
+		audio.set_pitch_scale(1.2)
+		audio.play()
 		isClosed = true
--- Scripts/Sound/music_manager.gd
+++ Scripts/Sound/music_manager.gd
@@ -1,56 +1,111 @@
 extends Node
 
-# All AudioStreamPlayers must be added as children
 var tracks: Array[AudioStreamPlayer] = []
-var fade_duration: float = 2.0
+var fade_duration: float = 4.0  # Dauer des Crossfades
 var target_volume: float = 0.0
 var off_volume: float = -80.0
 
+var player
+var currentTrack: AudioStreamPlayer
+var isInArea1: bool = false
+var isSwitching: bool = false
+var atBoss: bool = false
+
 func _ready():
-	for i in range(get_child_count()):
-		var child = get_child(i)
+	player = GlobalPlayer.getPlayer()
+
+	for i: int in range(get_child_count()):
+		var child := get_child(i)
 		if child is AudioStreamPlayer:
-			if tracks.is_empty():  # First track
-				child.volume_db = target_volume
-			else:
-				child.volume_db = off_volume
+			child.volume_db = off_volume
 			child.play()
 			tracks.append(child)
 
+	if tracks.size() > 0:
+		tracks[0].volume_db = target_volume
+		currentTrack = tracks[0]
 
-# Fade to the track by name or index
-func fade_to_track(index: int):
+
+func _physics_process(_delta: float) -> void:
+	if player.isDetected and !atBoss:
+		await switch_with_crossfade(2)
+		print("Battle")
+	elif isInArea1 and !atBoss:
+		await switch_with_crossfade(1)
+		print("Area1")
+	elif atBoss:
+		await switch_with_crossfade(3)
+		print("Boss")
+	else:
+		await switch_with_crossfade(0)
+		print("Area0")
+
+
+func switch_with_crossfade(index: int) -> void:
 	if index < 0 or index >= tracks.size():
 		return
+	if currentTrack == tracks[index] or isSwitching:
+		return
 
-	for i in tracks.size():
-		var player = tracks[i]
-		var target_db = target_volume if i == index else off_volume
-		_fade_volume(player, target_db, fade_duration)
+	isSwitching = true
+	var from_player: AudioStreamPlayer = currentTrack
+	var to_player: AudioStreamPlayer = tracks[index]
+	currentTrack = to_player
 
-# Internal volume fade coroutine
-func _fade_volume(player: AudioStreamPlayer, to_db: float, duration: float) -> void:
-	var from_db = player.volume_db
-	var t := 0.0
+	if not to_player.playing:
+		to_player.play()
+
+	await crossfade_tracks(from_player, to_player, fade_duration)
+
+	isSwitching = false
+
+
+func crossfade_tracks(from_player: AudioStreamPlayer, to_player: AudioStreamPlayer, duration: float) -> void:
+	var t: float = 0.0
+	
 	while t < duration:
 		t += get_process_delta_time()
-		var p = clamp(t / duration, 0, 1)
-		player.volume_db = lerp(from_db, to_db, p)
+		var ratio = clamp(t / duration, 0, 1)
+		
+		# Sinus/Cosinus Crossfade für konstante Gesamtamplitude
+		var amp_from = cos(ratio * PI * 0.5)
+		var amp_to = sin(ratio * PI * 0.5)
+		
+		from_player.volume_db = linear_to_db(amp_from)
+		to_player.volume_db = linear_to_db(amp_to)
+		
 		await get_tree().process_frame
-	player.volume_db = to_db
+	
+	from_player.volume_db = off_volume
+	to_player.volume_db = target_volume
+
+
+# Hilfsfunktionen als Methoden außerhalb von crossfade_tracks:
+
+func db_to_amp(db: float) -> float:
+	return pow(10, db / 20.0)
+
+func linear_to_db(amp: float) -> float:
+	return 20.0 * (log(amp) / log(10))
 
 
 
+
+# Trigger von Arealen
 func fade_to_Track0(area: Area3D) -> void:
 	if area.is_in_group("Player"):
-		fade_to_track(0)
-		
+		isInArea1 = false
+
 func fade_to_Track1(area: Area3D) -> void:
 	if area.is_in_group("Player"):
-		fade_to_track(1)
+		isInArea1 = true
+
 func fade_to_Track2(area: Area3D) -> void:
 	if area.is_in_group("Player"):
-		fade_to_track(2)
+		await switch_with_crossfade(2)
+
 func fade_to_Track3(area: Area3D) -> void:
 	if area.is_in_group("Player"):
-		fade_to_track(3)
+		isInArea1 = false
+		atBoss = true
+		await switch_with_crossfade(3)
